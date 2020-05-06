d8114463ff7b0eee31caaa2cd8b16a7022b78688
Parallels <zekaya@gmail.com>
2020-05-06 23:20:56 +0300

    Added independent buffer read thread.

---
+++ SpectrumMonitorApp/bufferReader.cpp
@@ -0,0 +1,131 @@
+#include "bufferReader.h"
+#include <algorithm>
+
+void BufferReader::stop()
+{
+    rcvr.shutdown();
+    qDebug() << "Receiver shutdown.";
+}
+
+void BufferReader::run()
+{
+    int ogBufferSize;
+    short* ogData;
+
+    data1 = (short*)malloc(2*bufferSize1*sizeof(short));
+    data2 = (short*)malloc(2*bufferSize2*sizeof(short));
+
+    ogData = data1;
+    currentDataPtr = 1;
+
+    rcvr.setStartupParams(MHZ(currentBW),MHZ(currentFS),MHZ(currentFC));
+    rcvr.CreateReceiver(URI,bufferSize1);
+    rcvr.updateBufferSize(bufferSize1);
+
+    qDebug() << "SDR Receiver has been created...";
+
+    while(threadActive)
+    {
+        if (currentDataPtr == 1)
+        {
+            if(data1)
+            {
+                ogData = data1;
+                ogBufferSize = bufferSize1;
+            }
+            else
+            {
+                qDebug() << " Data 1 array doesn't exist!";
+            }
+        }
+        else
+        {
+            if(data2)
+            {
+                ogData = data2;
+                ogBufferSize = bufferSize2;
+            }
+            else
+            {
+                qDebug() << " Data 2 array doesn't exist!";
+            }
+        }
+
+        // Read receive buffer
+        rcvr.pollRXBuffer(ogData);
+        emit sendToDemod(ogData,ogBufferSize);
+    }
+}
+
+void BufferReader::setBW(double bwval)
+{
+    currentBW = bwval;
+    rcvr.updateParams(MHZ(currentBW),MHZ(currentFS),MHZ(currentFC));
+}
+
+void BufferReader::setFS(double fsval)
+{
+    currentFS = fsval;
+    rcvr.updateParams(MHZ(currentBW),MHZ(currentFS),MHZ(currentFC));
+}
+
+void BufferReader::setFC(double fcval)
+{
+    currentFC = fcval;
+    rcvr.updateParams(MHZ(currentBW),MHZ(currentFS),MHZ(currentFC));
+}
+
+
+void BufferReader::setURI(char* urival)
+{
+    strncpy(URI,urival,sizeof(&urival));
+}
+
+void BufferReader::bwValueChanged(double newBWVal)
+{
+    setBW(newBWVal);
+}
+
+void BufferReader::fsValueChanged(double newFSVal)
+{
+    setFS(newFSVal);
+}
+
+void BufferReader::fcValueChanged(double newFCVal)
+{
+    setFC(newFCVal);
+}
+
+void BufferReader::bufferSizeChanged(int newBufSize)
+{
+    if(currentDataPtr == 1)
+    {
+        bufferSize2 = newBufSize;
+        free(data2);
+        data2 = (short*)malloc(2*bufferSize2*sizeof(short));
+        rcvr.updateBufferSize(bufferSize2);
+        currentDataPtr = 2;
+    }
+    else if(currentDataPtr == 2)
+    {
+        bufferSize1 = newBufSize;
+        free(data1);
+        data1 = (short*)malloc(2*bufferSize1*sizeof(short));
+        rcvr.updateBufferSize(bufferSize1);
+        currentDataPtr = 1;
+    }
+}
+
+void BufferReader::stopThread()
+{
+    threadActive = false;
+
+    if(data1)
+        free(data1);
+
+    if(data2)
+        free(data2);
+
+    quit();
+    stop();
+}
---
+++ SpectrumMonitorApp/bufferReader.h
@@ -0,0 +1,53 @@
+#ifndef BUFFERREADER_H
+#define BUFFERREADER_H
+
+#include <QThread>
+#include <QtDebug>
+
+#include <string>
+#include "fft.h"
+#include "pluto_receiver.h"
+//#include "adrv9009_receiver.h"
+#include "ui_mainwindow.h"
+
+class BufferReader : public QThread
+{
+    Q_OBJECT
+
+public:
+    void stop();
+    void setBW(double bwval);
+    void setFS(double fsval);
+    void setFC(double fcval);
+    void setURI(char* urival);
+
+signals:
+    void sendToDemod(short* data, int size);
+
+private slots:
+    void bwValueChanged(double newBWVal);
+    void fsValueChanged(double newFSVal);
+    void fcValueChanged(double newFCVal);
+    void bufferSizeChanged(int newFCVal);
+    void stopThread();
+
+private:
+//    ADRV9009Receiver rcvr;
+    PlutoReceiver rcvr;
+    double currentBW = 1;
+    double currentFS = 2.4;
+    double currentFC = 99.796;
+    char URI[15] = "ip:192.168.2.1";
+    short* data1;
+    short* data2;
+    int currentDataPtr = 1;
+    bool threadActive = true;
+
+    int bufferSize1 = 65536;
+    int bufferSize2 = 65536;
+
+    void run();
+
+};
+
+#endif // BUFFERREADER_H
